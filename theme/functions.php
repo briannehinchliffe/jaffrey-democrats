51b9c874ebe42b5a499acacf4aca3218110ad997
Brianne Hinchliffe <brianne@paragondigital.com>
2026-09-12 21:32:51 -0400

    Add: Button Variations

--- theme/functions.php
+++ theme/functions.php
@@ -204,23 +204,30 @@ function jaffrey_democrats_tinymce_add_class( $settings ) {
 add_filter( 'tiny_mce_before_init', 'jaffrey_democrats_tinymce_add_class' );
 
 /**
- * Limit the block editor to heading levels supported by Tailwind Typography.
+ * Register block styles.
  *
- * @param array  $args Array of arguments for registering a block type.
- * @param string $block_type Block type name including namespace.
- * @return array
+ * @return void
  */
-function jaffrey_democrats_modify_heading_levels( $args, $block_type ) {
-	if ( 'core/heading' !== $block_type ) {
-		return $args;
-	}
+function jaffrey_democrats_register_block_styles() {
+    register_block_style(
+        'core/button',
+        array(
+            'name'  => 'fill-red',
+            'label' => __( 'Red Fill', 'jaffrey-democrats' ),
+        )
+    );
 
-	// Remove <h1>, <h5> and <h6>.
-	$args['attributes']['levelOptions']['default'] = array( 2, 3, 4 );
+    register_block_style(
+        'core/button',
+        array(
+            'name'  => 'fill-opaque',
+            'label' => __( 'Opaque Fill', 'jaffrey-democrats' ),
+        )
+    );
 
-	return $args;
+
 }
-add_filter( 'register_block_type_args', 'jaffrey_democrats_modify_heading_levels', 10, 2 );
+add_action( 'init', 'jaffrey_democrats_register_block_styles' );
 
 /**
  * Register a custom Countdown Timer block.
